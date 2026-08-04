d5195e1f40acb351974663ecc93cebf8c2ed92ad
famisics <dev@uiro.dev>
2026-08-04 21:51:50 +0900

    reminder を移行して不要なファイルを削除

--- services/connections/discord-bot/fun-council/main.go
+++ services/connections/discord-bot/fun-council/main.go
@@ -7,7 +7,7 @@ import (
   "syscall"
 
   "github.com/bwmarrin/discordgo"
-  "github.com/famisics/fynsv/services/connections/shared/discord-bot/reminder"
+  "github.com/famisics/fynsv/services/connections/discord-bot/fun-council/reminder"
 )
 
 func main() {
